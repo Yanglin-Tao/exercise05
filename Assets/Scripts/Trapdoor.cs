d5dcd27ed7dcf899e643260d4490df28b1bb82c2
Justin Rivera <50186921+jriv01@users.noreply.github.com>
2023-02-27 01:05:11 -0500

    Added trapdoor script

---
+++ Assets/Scripts/Trapdoor.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trapdoor : MonoBehaviour
+{   
+    public float fallTimer;
+    private float currentTimer;
+    private bool playerFall;
+    Renderer renderer;
+    void Start() {
+        // Setting initial parameters
+        currentTimer = fallTimer;
+        playerFall = false;
+
+        // Getting the render to show progression
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update() {
+        // If the player is on the trapdoor, begin the timer
+        if(playerFall) {
+            // Update the timer
+            currentTimer -= Time.deltaTime;
+            // Update visuals
+            float ratio = currentTimer / fallTimer;
+            renderer.material.SetColor("_Color", new Color(1f - ratio, ratio, 0));
+
+            // If the timer runs out, drop the player
+            if(currentTimer <= 0f) {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+
+    void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Player")) {
+            playerFall = true;
+        }
+    }
+
+
+    void OnCollisionExit(Collision other) {
+        if(other.gameObject.CompareTag("Player")) {
+            playerFall = false;
+            currentTimer = fallTimer;
+            renderer.material.SetColor("_Color", new Color(0f, 1f, 0f, 1f));
+        }
+    }
+}
