6de55cf109121590a256d4c5ae1c829c36848c61
Remi <70982674+Remi-NYU@users.noreply.github.com>
2023-03-09 13:26:32 -0500

    Fixed player controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,34 +6,37 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    NavMeshAgent _navMeshAgent;
-    Camera mainCam;
-    Rigidbody _rigidbody;
+	NavMeshAgent _navMeshAgent;
+	Camera mainCam;
+	Rigidbody _rigidbody;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        _rigidbody = GetComponent<Rigidbody>();
-        _navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        mainCam = Camera.main;
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+		_navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		mainCam = Camera.main;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButton(0)){
-            _navMeshAgent.enabled = true;
-            RaycastHit hit;
-            if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 200)){
-                _navMeshAgent.destination = hit.point;
-            }
-        }
-        
-        if (!Input.GetMouseButton(0)){
-            _navMeshAgent.enabled = false;
-        }
-    }
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			_navMeshAgent.enabled = true;
+			RaycastHit hit;
+			if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 200))
+			{
+				_navMeshAgent.destination = hit.point;
+			}
+		}
+
+		if (!Input.GetMouseButton(0))
+		{
+			_navMeshAgent.enabled = false;
+		}
+	}
 }
 
